bef820ba960a60a56432279bce6ca632ed8e3d73
Pablo Valdez <patriciovaldez944@gmail.com>
2021-12-13 12:58:29 -0800

    vercion2 agregando funcion saludar alerta js

---
+++ js/saludar.js
@@ -0,0 +1,12 @@
+function saludar(){
+    respuesta=prompt("hola como estas ")
+    if (respuesta=="bien") {
+        alert("Me alegro")
+        
+    } else {
+        alert("Una pena")
+        
+    }
+}
+
+saludar()
